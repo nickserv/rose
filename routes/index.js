2b067a765cdf5818a3917f3850f8a19724704547
Nicolas McCurdy <thenickperson@gmail.com>
2014-01-12 05:54:46 -0500

    Implement basic search for commands (does not translate yet)

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,34 @@
  * GET home page.
  */
 
+var featuresJSON = require("../data.json");
+
+function getFeatures() {
+  return Object.keys(featuresJSON).reduce(function (memo, technology) {
+    featuresJSON[technology].forEach(function (data) {
+      memo.push({
+        technology: technology,
+        name: data
+      });
+    });
+    return memo;
+  }, []);
+}
+
+function findFeatures(query) {
+  if(query) {
+    return getFeatures().filter(function (data) {
+      return data.name.indexOf(query) > -1;
+    });
+    //return new Array(_.findWhere(exports.getLibraries(), { name: names }));
+  } else {
+    return getFeatures();
+  }
+}
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Rose' });
+  res.render('index', {
+    title: 'Rose',
+    results: findFeatures(req.query.query)
+  });
 };
