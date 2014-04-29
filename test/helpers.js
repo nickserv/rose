d92c6a6a29a5f599326cb749ba3d139a9ef7056c
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-29 15:43:54 -0400

    Add pending specs for helpers.toArray()

---
+++ test/helpers.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var seeds = require('../helpers');
+
+describe('helpers', function () {
+  describe('toArray', function () {
+    context('when given an array', function () {
+      it('returns the unmodified array');
+    });
+
+    context('when given an item', function () {
+      it('returns an array of length 1 containing the item');
+    });
+  });
+});
