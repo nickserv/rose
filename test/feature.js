91d44c4959e3c94ca870c77f3910b72513c568d1
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-29 14:11:16 -0400

    Add specs for Feature.search()

---
+++ test/feature.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+var Feature = require('../feature');
+
+describe('Feature', function () {
+  describe('schema', function () {
+    it('successfully creates a valid document');
+    it('fails at creating an invalid document');
+  });
+
+  describe('.search()', function () {
+    it('performs an empty search, returning all commands', function (done) {
+      Feature.search('', function (docs) {
+        assert.equal(7, docs.length);
+        done();
+      });
+    });
+
+    it('performs a case-insensitive search for a command', function (done) {
+      Feature.search('git ADD', function (docs) {
+        assert.equal(1, docs.length)
+        done();
+      });
+    });
+
+    it('performs a search for a command that does not exist', function (done) {
+      Feature.search('git yolo', function (docs) {
+        assert.equal(0, docs.length);
+        done();
+      });
+    });
+  });
+});
