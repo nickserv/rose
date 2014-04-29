9ad9102471f5ad251cc9defcde4d08d76aa4b19b
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-29 15:37:37 -0400

    Add pending specs for seeds

---
+++ test/seeds.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+var seeds = require('../seeds');
+
+describe('seeds', function () {
+  it('clears the database');
+  it('adds the correct number of documents to the features collection');
+});
