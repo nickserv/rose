2d0331a779637ac8a8ca50635ea6b4401a49b207
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-29 05:10:52 -0400

    Add log messages about database seeding progress

--- seeds.js
+++ seeds.js
@@ -2,10 +2,12 @@ var seeds = require('./features.json');
 var Feature = require('./feature');
 
 module.exports = function (callback) {
+  console.log('Seeding the database...')
   // Drop the features collection.
   Feature.remove(function () {
     // Seed the features collection.
     Feature.create(seeds, function () {
+      console.log('Seeding complete.')
       callback();
     });
   });
