fcb97c3507b9c22cece1cae545dda02d213ca839
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-29 04:29:44 -0400

    Rename features.js to feature.js (to prepare for a refactor)

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-var features = require('../features');
+var feature = require('../feature');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  features.find(req.query.query, function (docs) {
+  feature.find(req.query.query, function (docs) {
     res.render('index', {
       title: 'Rose',
       features: docs,
       query: req.query.query,
-      toArray: features.toArray
+      toArray: feature.toArray
     });
   });
 });
