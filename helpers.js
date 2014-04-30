9887afa1e9756140274df2894f74bcd60192fd8e
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-30 12:35:43 -0400

    Remove helpers.contains() and helpers.someContain()

--- helpers.js
+++ helpers.js
@@ -1,21 +1,5 @@
-// ignoreCase default to true
-function contains(string, query, ignoreCase) {
-  if (ignoreCase !== false) {
-    string = string.toLowerCase();
-    query = query.toLowerCase();
-  }
-  return string.indexOf(query) !== -1;
-}
-
-// ignoreCase default to true
-function someContain(strings, query, ignoreCase) {
-  return strings.some(function (string) {
-    return contains(string, query, ignoreCase);
-  });
-}
-
 function toArray(item) {
   return (item instanceof Array) ? item : [item];
 }
 
-module.exports = { contains: contains, someContain: someContain, toArray: toArray };
+module.exports = { toArray: toArray };
