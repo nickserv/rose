6740fc477edffbef4154f0f1f3082fc70f4f8cc4
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-30 02:47:06 -0400

    Implement specs for helpers.toArray()

--- test/helpers.js
+++ test/helpers.js
@@ -1,14 +1,18 @@
 var assert = require('assert');
-var seeds = require('../helpers');
+var helpers = require('../helpers');
 
 describe('helpers', function () {
-  describe('toArray', function () {
+  describe('.toArray()', function () {
     context('when given an array', function () {
-      it('returns the unmodified array');
+      it('returns the unmodified array', function () {
+        assert.deepEqual(helpers.toArray([1, 2, 3]), [1, 2, 3]);
+      });
     });
 
     context('when given an item', function () {
-      it('returns an array of length 1 containing the item');
+      it('returns an array of length 1 containing the item', function () {
+        assert.deepEqual(helpers.toArray('item'), ['item']);
+      });
     });
   });
 });
