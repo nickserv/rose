f3fb1352d9b5f78360a85daab2398f8f81d9b97c
Nicolas McCurdy <thenickperson@gmail.com>
2014-05-01 03:06:29 -0400

    Use Promises for seeds()

--- seeds.js
+++ seeds.js
@@ -2,14 +2,14 @@ var debug = require('debug')('rose');
 var seeds = require('./seedData');
 var Feature = require('./feature');
 
-module.exports = function (callback) {
+module.exports = function () {
   debug('Seeding the database...');
   // Drop the features collection.
-  Feature.remove(function () {
-    // Seed the features collection.
-    Feature.create(seeds, function () {
-      debug('Seeding complete.');
-      callback();
-    });
-  });
+  return Feature.remove()
+                .exec(function () {
+                  // Seed the features collection.
+                  return Feature.create(seeds, function () {
+                    debug('Seeding complete.');
+                  });
+                });
 };
