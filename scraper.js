1d61666f8de566b9b433a5f07e2932ad23d61304
Nicolas McCurdy <thenickperson@gmail.com>
2014-11-03 09:35:35 -0500

    Add comments to the scraper

--- scraper.js
+++ scraper.js
@@ -6,26 +6,31 @@ module.exports = function () {
   var promise = new Promise;
 
   request('http://hyperpolyglot.org/version-control', function (error, response, body) {
-    if (!error && response.statusCode === 200) {
+    if (!error) {
       var features = [];
-
       var $ = cheerio.load(body);
       var technologies = [];
 
+      // Find the first table.
       $('.wiki-content-table').first().find('tr').each(function (index) {
         if (index === 1) {
           $(this).find('th').each(function (index) {
             if (index > 0) {
+              // Get the names of the technologies, removing the years.
               var technology = $(this).text().replace(/\s*\(\d{4}\)/, '');
               technologies.push(technology);
             }
           });
         } else if (index > 1) {
+          // Create a feature.
           var feature = { examples: [] };
           $(this).find('td').each(function (index) {
+            // Get feature data.
             if (index === 0) {
+              // Get the name of the feature.
               feature.name = $(this).text();
             } else if ($(this).text()) {
+              // Collect the cell data as one example.
               feature.examples.push({
                 technology: technologies[index - 1],
                 snippets: $(this).text()
@@ -36,6 +41,7 @@ module.exports = function () {
         }
       });
 
+      // Fulfill the promise.
       promise.fulfill(features);
     }
   });
