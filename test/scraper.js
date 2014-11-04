8321d18d8e5ef49d8683bcf1cf3bedcc4a630855
Nicolas McCurdy <thenickperson@gmail.com>
2014-11-04 13:15:58 -0500

    Add tests for most of the other scraping functions

--- test/scraper.js
+++ test/scraper.js
@@ -1,26 +1,61 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+var expect = require('expect.js');
 var scraper = require('../scraper');
 var Feature = require('../feature');
 var SECOND = 1000;
 
 describe('scraper', function () {
+  before(function () {
+    var tableHTML = fs.readFileSync(__dirname + '/table.html', 'utf8');
+    this.$ = cheerio.load(tableHTML);
+
+    this.expectedTechnologies = ['git', 'mercurial'];
+    this.expectedFeatures = [{
+      name: 'add files',
+      examples: [
+        { technology: 'git', snippets: 'git add' },
+        { technology: 'mercurial', snippets: 'hg add' }
+      ]
+    }, {
+      name: 'show revision information line by line',
+      examples: [
+        { technology: 'git', snippets: 'git blame' },
+        { technology: 'mercurial', snippets: 'hg annotate' }
+      ]
+    }];
+  });
+
   context('.scrapeTechnologies()', function () {
-    xit('has tests');
+    it('scrapes all technology names from a table header row', function () {
+      expect(scraper.scrapeTechnologies(this.$('tr').eq(1), this.$)).to.eql(this.expectedTechnologies);
+    });
   });
 
   context('.scrapeFeature()', function () {
-    xit('has tests');
+    it('scrapes a feature from a table row', function () {
+      expect(scraper.scrapeFeature(this.$('tr').eq(2), this.$, this.expectedTechnologies)).to.eql(this.expectedFeatures[0]);
+    });
   });
 
   context('.scrapeTable()', function () {
-    xit('has tests');
+    it('scrapes all features from a table', function () {
+      expect(scraper.scrapeTable(this.$('table'), this.$)).to.eql(this.expectedFeatures);
+    });
   });
 
   context('.requestPromise()', function () {
-    xit('has tests');
+    context('after a successful request', function () {
+      xit('wraps request() and returns a Promise that will be resolved');
+    });
+
+    context('after a failed request', function () {
+      xit('wraps request() and returns a Promise that will be rejected');
+    });
   });
 
   context('.scrape()', function () {
-    it('generates JSON for valid Features', function (done) {
+    it('scrapes features from the Hyperpolyglot website', function (done) {
       this.timeout(10 * SECOND);
       scraper.scrape().then(function (seeds) {
         seeds.forEach(function (seed, index, array) {
