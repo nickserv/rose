d23cdcef2f9e9249444ee5cbf073eb240b0cca3d
Nicolas McCurdy <thenickperson@gmail.com>
2014-11-04 10:30:51 -0500

    Add skeletons for more scraper module tests

--- test/scraper.js
+++ test/scraper.js
@@ -3,16 +3,34 @@ var Feature = require('../feature');
 var SECOND = 1000;
 
 describe('scraper', function () {
-  it('generates JSON for valid Features', function (done) {
-    this.timeout(10 * SECOND);
-    scraper.scrape().then(function (seeds) {
-      seeds.forEach(function (seed, index, array) {
-        (new Feature(seed)).validate(function (err) {
-          if (err) {
-            done(err);
-          } else if (index === array.length - 1) {
-            done();
-          }
+  context('.scrapeTechnologies()', function () {
+    xit('has tests');
+  });
+
+  context('.scrapeFeature()', function () {
+    xit('has tests');
+  });
+
+  context('.scrapeTable()', function () {
+    xit('has tests');
+  });
+
+  context('.requestPromise()', function () {
+    xit('has tests');
+  });
+
+  context('.scrape()', function () {
+    it('generates JSON for valid Features', function (done) {
+      this.timeout(10 * SECOND);
+      scraper.scrape().then(function (seeds) {
+        seeds.forEach(function (seed, index, array) {
+          (new Feature(seed)).validate(function (err) {
+            if (err) {
+              done(err);
+            } else if (index === array.length - 1) {
+              done();
+            }
+          });
         });
       });
     });
