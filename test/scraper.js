b74495932b4b714aaaea0e9b022bc3b463c12370
Nicolas McCurdy <thenickperson@gmail.com>
2014-11-04 09:50:37 -0500

    Increase the timeout for the scraper.scrape() example

--- test/scraper.js
+++ test/scraper.js
@@ -1,8 +1,10 @@
 var scraper = require('../scraper');
 var Feature = require('../feature');
+var SECOND = 1000;
 
 describe('scraper', function () {
   it('generates JSON for valid Features', function (done) {
+    this.timeout(10 * SECOND);
     scraper.scrape().then(function (seeds) {
       seeds.forEach(function (seed, index, array) {
         (new Feature(seed)).validate(function (err) {
