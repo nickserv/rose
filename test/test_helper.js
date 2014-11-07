87f6cc8885bed9e535955f413dcfd4e02ad41cd9
Nicolas McCurdy <thenickperson@gmail.com>
2014-11-07 12:42:12 -0500

    Add a test helper to make test imports more DRY
    Conflicts:
    	test/scraper.js

---
+++ test/test_helper.js
@@ -0,0 +1,7 @@
+// Testing libraries
+global.expect = require('chai').expect;
+
+// Rose modules
+global.Feature = require('../feature');
+global.helpers = require('../helpers');
+global.seeds = require('../seeds');
