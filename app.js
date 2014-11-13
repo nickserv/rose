b18ec43ff935c6c82c307ba0075db15d4a7e41ac
Nicolas McCurdy <thenickperson@gmail.com>
2014-11-13 08:35:43 -0500

    Fix code style in app.js

--- app.js
+++ app.js
@@ -14,22 +14,22 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 /* GET home page. */
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('index');
 });
 
 /* GET JSON API. */
-app.get('/index.json', function(req, res) {
+app.get('/index.json', function (req, res) {
   Feature.search(req.query.query).then(function (docs) {
     res.json(docs);
   });
 });
 
 /// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
@@ -37,23 +37,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function (err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use(function (err, req, res) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
