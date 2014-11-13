0190f95cd9af5f670b7c774411d0ce20853ca89d
Nicolas McCurdy <thenickperson@gmail.com>
2014-11-12 21:03:29 -0500

    Add basic tests for the app itself

---
+++ test/app.js
@@ -0,0 +1,25 @@
+require('./test_helper');
+
+describe('app', function () {
+  describe('GET /', function () {
+    it('responds with success', function (done) {
+      request(app)
+        .get('/')
+        .expect(200)
+        .end(done);
+    });
+  });
+
+  describe('GET /index.json', function () {
+    it('responds with features', function (done) {
+      request(app)
+        .get('/index.json')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          expect(res.body).to.have.length.above(0);
+        })
+        .end(done);
+    });
+  });
+});
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,8 @@
 // Testing libraries
 global.expect = require('chai').expect;
+global.request = require('supertest');
 
 // Rose modules
+global.app = require('../app');
 global.Feature = require('../models/feature');
 global.seeds = require('../models/seeds');
