b1537da1b1a6de1c4fa7b358d136edd6732912a7
Nicolas McCurdy <thenickperson@gmail.com>
2016-01-26 02:15:10 -0500

    Start porting the app from Angular to React

---
+++ public/client.jsx
@@ -0,0 +1,75 @@
+function Example (props) {
+  return <tr>
+    <td>{props.technology}</td>
+    <td>
+      {props.snippets.map(snippet =>
+        <pre key={snippet}><code>{snippet}</code></pre>
+      )}
+    </td>
+  </tr>
+}
+
+function Feature (props) {
+  return <li className="card">
+    <h2 className="panel-title">{props.name}</h2>
+
+    <div className="table-responsive">
+      <table>
+        <tbody>
+          {props.examples.map(example =>
+            <Example key={example.technology} technology={example.technology} snippets={example.snippets}/>
+          )}
+        </tbody>
+      </table>
+    </div>
+  </li>
+}
+
+function SearchResults(props) {
+  if (props.results.length > 0) {
+    return <ol className="search-results">
+      {props.results.map(result =>
+        <Feature key={JSON.stringify(result)} name={result.name} examples={result.examples}/>
+      )}
+    </ol>;
+  } else {
+    return <h2>No results</h2>;
+  }
+}
+
+class SearchBar extends React.Component {
+  search(event) {
+    this.props.search(event.target.value);
+  }
+
+  render() {
+    return <input id="query" type="search" className="search-input" value={this.props.query} placeholder="Search for features, technologies, or code snippets" autofocus onChange={this.search.bind(this)}/>
+  }
+}
+
+class Searchable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { results: [] };
+  }
+
+  componentDidMount() {
+    this.search(this.props.query);
+  }
+
+  search(query) {
+    if (query === undefined) query = ''
+    fetch('/index.json?query=' + query + '&index=0&count=10')
+      .then(response => response.json())
+      .then(results => this.setState({ results }))
+  }
+
+  render() {
+    return <div>
+      <SearchBar search={this.search.bind(this)}/>
+      <SearchResults results={this.state.results}/>
+    </div>
+  }
+}
+
+ReactDOM.render(<Searchable/>, searchable);
