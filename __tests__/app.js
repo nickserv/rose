b3457d669cbbc3b1e0c68bd4f38bfc89f3ffe6a9
Nick McCurdy <nick@nickmccurdy.com>
2017-05-05 05:50:17 -0400

    Replace supertest with request

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,34 +1,42 @@
 const app = require('../app');
-const request = require('supertest');
+const request = require('request');
 const seeds = require('../lib/seeds');
 const seedData = require('./seedData');
 
 describe('app', () => {
   beforeAll(() => seeds(seedData));
+  beforeAll(done => app.listen(3000, done));
 
   describe('GET /', () => {
-    it('responds with success', () => {
-      return request(app)
-        .get('/')
-        .expect(200);
+    it('responds with success', done => {
+      request('http://localhost:3000/', (error, response) => {
+        expect(error).toBeNull();
+        expect(response.statusCode).toBe(200);
+        done();
+      });
     });
   });
 
   describe('GET /index.json', () => {
-    it('responds with features', () => {
-      return request(app)
-        .get('/index.json')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .expect(res => expect(res.body).not.toHaveLength(0));
+    it('responds with features', done => {
+      request('http://localhost:3000/index.json', {
+        headers: { 'Content-Type': /json/ }
+      }, (error, response, body) => {
+        expect(error).toBeNull();
+        expect(response.statusCode).toBe(200);
+        expect(body.length).toBeGreaterThan(0);
+        done();
+      });
     });
   });
 
   describe('GET /404', () => {
-    it('responds with a 404 error', () => {
-      return request(app)
-        .get('/404')
-        .expect(404);
+    it('responds with a 404 error', done => {
+      request('http://localhost:3000/404', (error, response) => {
+        expect(error).toBeNull();
+        expect(response.statusCode).toBe(404);
+        done();
+      });
     });
   });
 });
