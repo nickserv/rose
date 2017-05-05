2a42d58b32afe3199fdfd5f3f0dbb84a7d037112
Nick McCurdy <nick@nickmccurdy.com>
2017-05-05 06:23:23 -0400

    Use request.defaults() to simplify testing

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,21 +1,23 @@
 const app = require('../app');
-const request = require('request-promise-native');
+const requestPromiseNative = require('request-promise-native');
 const seeds = require('../lib/seeds');
 const seedData = require('./seedData');
 
 describe('app', () => {
+  const request = requestPromiseNative.defaults({ baseUrl: 'http://localhost:3000/' });
+
   beforeAll(() => seeds(seedData));
   beforeAll(done => app.listen(3000, done));
 
   describe('GET /', () => {
     it('responds with success', () => {
-      return request('http://localhost:3000/');
+      return request('/');
     });
   });
 
   describe('GET /index.json', () => {
     it('responds with features', () => {
-      return request('http://localhost:3000/index.json', {
+      return request('/index.json', {
         headers: { 'Content-Type': /json/ }
       }).then(response => expect(response.length).toBeGreaterThan(0));
     });
@@ -23,7 +25,7 @@ describe('app', () => {
 
   describe('GET /404', () => {
     it('responds with a 404 error', () => {
-      return request('http://localhost:3000/404', {
+      return request('/404', {
         resolveWithFullResponse: true,
         simple: false
       }).then(response => expect(response.statusCode).toBe(404));
