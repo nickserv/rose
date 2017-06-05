60d7c198edb42351bc9d7c3bf5d16b7b648af046
Nick McCurdy <nick@nickmccurdy.com>
2017-06-05 05:08:41 -0400

    Clean up server requires

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
+const engine = require('./engine');
 const express = require('express');
-const path = require('path');
+const features = require('../dist/features');
 const logger = require('morgan');
 const webpack = require('webpack');
 const webpackConfig = require('../webpack.config');
 const webpackMiddleware = require('webpack-dev-middleware');
-
-const engine = require('./engine');
-const features = require('../dist/features');
 
 const app = express();
 
