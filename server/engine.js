83ce4f081aca0aa78aaa906f69294121036b3e61
Nick McCurdy <nick@nickmccurdy.com>
2017-06-08 13:01:20 -0400

    Fix engine function references

--- server/engine.js
+++ server/engine.js
@@ -4,15 +4,15 @@ export function matchSnippet(snippet, query) {
 
 export function matchExample(example, query) {
   return example.technology.match(query) ||
-    this.matchSnippet(example.snippet, query);
+    matchSnippet(example.snippet, query);
 }
 
 export function matchFeature(feature, query) {
   return feature.name.match(query) ||
-    feature.examples.some(example => this.matchExample(example, query));
+    feature.examples.some(example => matchExample(example, query));
 }
 
 export function search(features, query) {
   const queryRegExp = new RegExp(query, 'i');
-  return features.filter(feature => this.matchFeature(feature, queryRegExp));
+  return features.filter(feature => matchFeature(feature, queryRegExp));
 }
