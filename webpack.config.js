78f5cddbe3fb4d69d9eb469273d78b8dfc4ee9bc
Nick McCurdy <nick@nickmccurdy.com>
2017-06-26 04:08:41 -0400

    Simplify loader usage

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,7 @@ module.exports = [
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader'
-          }
+          use: 'babel-loader'
         },
         {
           test: /\.less$/,
